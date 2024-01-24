e7bdf6a84729b8a6773e8e367a29f69fcdd71d22
jaalmario <jovanniealmario@gmail.com>
2024-01-24 14:50:06 +0800

    Initial creation for base viewsets

---
+++ game/api/base_viewset.py
@@ -0,0 +1,38 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+
+class BaseViewSet(viewsets.ViewSet):
+    """Allows basic operations like list, retrieve, delete, """
+    """ when using this provide:        """
+    """ - queryset                      """
+    """ - serializer_class              """
+    queryset = ()
+    serializer_class = ()
+
+    def list(self, request):
+        # serializer = CompanyGameSerializer(self.queryset, many=True)
+        serializer = self.serializer_class(self.queryset, many=True)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        instance = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(instance)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['put'])
+    def delete(self, request, pk=None):
+        instance = get_object_or_404(self.queryset, pk=pk)
+        instance.isDeleted = True
+        serializer = self.serializer_class(instance)
+        serializer.save()
+        return JsonResponse(serializer.data)
+    
+
--- game/models/company_game.py
+++ game/models/company_game.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from game.models.game import Game
+from game.models.game import Game
 
 class CompanyGame(models.Model):
     """Model definition for CompanyGame."""
@@ -8,7 +8,7 @@ class CompanyGame(models.Model):
         return {"min_bet": 25}
     
     companyId = models.CharField(max_length=40)
-    # gameId = models.ForeignKey(Game, verbose_name=("GameId"), on_delete=models.CASCADE)
+    gameId = models.ForeignKey(Game, verbose_name=("GameId"), on_delete=models.CASCADE)
     gameSettings = models.JSONField("GameSettings", default=game_settings_default)
     isDeleted= models.BooleanField(default=False)
 
