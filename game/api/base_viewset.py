580d0e40bdcf3bb7d06a38587abc3a90ae623913
jaalmario <jovanniealmario@gmail.com>
2024-01-25 12:23:04 +0800

    remove unused import

--- game/api/base_viewset.py
+++ game/api/base_viewset.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from game.models import CompanyGame
 
 class BaseViewSet(viewsets.ViewSet):
     """Allows basic operations like list, retrieve, delete, """
