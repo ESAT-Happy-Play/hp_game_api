cbb1005cb0f672d46aa39c5c943a5457804cf5fc
jaalmario <jovanniealmario@gmail.com>
2024-01-26 12:10:35 +0800

    Remove commented betTransaction field in bet item

--- game/models/bet_item.py
+++ game/models/bet_item.py
@@ -1,7 +1,7 @@
 from django.db import models
 from game.models.company_game import CompanyGame
 from game.models.game_schedule import GameSchedule
-# from game.models.bet_transaction import BetTransaction
+from game.models.bet_transaction import BetTransaction
 
 class BetItem(models.Model):
     """Model definition for BetItem."""
@@ -9,7 +9,7 @@ class BetItem(models.Model):
     value = models.IntegerField()
     orderItemId = models.BigIntegerField()
     amount = models.IntegerField()
-    # betTransaction = models.ForeignKey(BetTransaction, verbose_name=("BetTransaction"), on_delete=models.CASCADE)
+    betTransaction = models.ForeignKey(BetTransaction, verbose_name=("BetTransaction"), on_delete=models.CASCADE)
     companyGame = models.ForeignKey(CompanyGame, verbose_name=("CompanyGame"), on_delete=models.CASCADE)
     gameSchedule = models.ForeignKey(GameSchedule, verbose_name=("GameSchedule"), on_delete=models.CASCADE)
     transactionDate = models.DateField()
