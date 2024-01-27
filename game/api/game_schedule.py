994e7b52669407d93bb6060cb3f54307599f56d2
Krishia <krishiaranquebello@gmail.com>
2024-01-27 13:53:50 +0800

    implement get total bet amount per schedule

--- game/api/game_schedule.py
+++ game/api/game_schedule.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-from django.db.models import Count
+from django.db.models import Count, Sum
 
 class GameScheduleViewSet(BaseViewSet):
     queryset = GameSchedule.objects.filter(isDeleted=False)
@@ -17,7 +17,7 @@ class GameScheduleViewSet(BaseViewSet):
         Get the combination percentage of the draw given gameScheduleId.
         """
         game_schedule = get_object_or_404(self.queryset, pk=pk)
-        bet_items = BetItem.objects.filter(gameSchedule=game_schedule)
+        bet_items = BetItem.objects.filter(gameSchedule=game_schedule, isDeleted=False)
         total_unique_values = bet_items.values('value').distinct().count()
         company_game = game_schedule.companyGame
         unique_combinations = company_game.gameSettings.get('unique_combinations', 0) # should have unique_combinations on the gameSettings JSONB data
@@ -29,4 +29,16 @@ class GameScheduleViewSet(BaseViewSet):
           combination_percentage = (total_unique_values / unique_combinations) * 100
 
         data = {'combination_percentage': combination_percentage}
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['get'])
+    def total_bet_amount(self, request, pk=None):
+        """
+        Get total amount of bets given gameScheduleId.
+        """
+        game_schedule = get_object_or_404(self.queryset, pk=pk)
+        bet_items = BetItem.objects.filter(gameSchedule=game_schedule, isDeleted=False)
+        total_amount = bet_items.aggregate(Sum('amount'))['amount__sum'] or 0
+
+        data = {'total_bet_amount': total_amount}
         return JsonResponse(data, status=status.HTTP_200_OK)
