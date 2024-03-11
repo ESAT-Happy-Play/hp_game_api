4588208fab017c1ccd16a42f6b94579235103833
Krishia <krishiaranquebello@gmail.com>
2024-03-11 09:27:17 +0800

    remove read only gameDrawType on gameschedules

--- game/serializers/game_schedule.py
+++ game/serializers/game_schedule.py
@@ -3,7 +3,6 @@ from game.models import GameSchedule
 from .game_draw_type import GameDrawTypeSerializer
 
 class GameScheduleSerializer(serializers.ModelSerializer):
-	gameDrawType = GameDrawTypeSerializer(read_only=True)
 	class Meta:
 		model = GameSchedule
 		fields = '__all__'
