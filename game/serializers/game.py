9f9a4a47e066e83eca495498e491cbafc8cbdc30
Krishia <krishiaranquebello@gmail.com>
2024-03-13 10:26:57 +0800

    add create game validation for gameMechanics required fields

--- game/serializers/game.py
+++ game/serializers/game.py
@@ -2,11 +2,22 @@ from rest_framework import serializers
 from game.models import Game
 
 class GameSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Game
-		fields = '__all__'
-		
-        
+    class Meta:
+      model = Game
+      fields = '__all__'
+
+    def validate(self, data):
+        game_mechanics = data.get('gameMechanics', {})
+        n_cards = game_mechanics.get('n_cards')
+        n_suites = game_mechanics.get('n_suites')
+
+        if n_cards is None:
+            raise serializers.ValidationError("n_cards field is required")
+        if n_suites is None and 'n_suites' not in game_mechanics:
+            raise serializers.ValidationError("n_suites field is required")
+
+        return data
+
 class GameCreateSerializer(GameSerializer):
     class Meta:
         model = Game
