af2a1b8bb3f75e5c730bfb8ba91c563c7fb4dc93
Krishia <krishiaranquebello@gmail.com>
2024-04-02 17:16:35 +0800

    implement has-winner endpoint and total bet count data

--- game/api/prize_pool.py
+++ game/api/prize_pool.py
@@ -1,10 +1,11 @@
 from game.serializers import PrizePoolSerializer, PrizePoolCreateSerializer
-from game.models import PrizePool
+from game.models import PrizePool, DrawResult
 from .base_viewset import BaseViewSet
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.decorators import action
 from django.http import JsonResponse
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class PrizePoolViewSet(BaseViewSet):
     queryset = PrizePool.objects.filter(isDeleted=False)
@@ -41,3 +42,21 @@ class PrizePoolViewSet(BaseViewSet):
 
         serializer = self.serializer_class(latest_prize_pool)
         return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(parameters=[
+        OpenApiParameter(name='companyGameId', description='Company Game ID', type=int, location=OpenApiParameter.PATH, required=True),
+    ],
+    operation_id='get_if_prize_pool_has_winners')
+    @action(detail=False, methods=['get'], url_path='(?P<companyGameId>[^/.]+)/latest/has-winners')
+    def get_if_prize_pool_has_winners(self, request, companyGameId):
+        latest_prize_pool = self.queryset.filter(companyGame_id=companyGameId, isDeleted=False).order_by('-id').first()
+        if latest_prize_pool is None:
+            return JsonResponse({'detail': 'No prize pool found for the specified company game ID.'}, status=status.HTTP_404_NOT_FOUND)
+
+        draw_result = DrawResult.objects.filter(gameSchedule__exact=latest_prize_pool.gameSchedule_id, isDeleted=False).first()
+        if draw_result is None:
+            return JsonResponse({'detail': 'No draw result yet, cannot determine if has winners.'}, status=status.HTTP_404_NOT_FOUND)
+
+        has_winners = draw_result.noOfWinners is not None and draw_result.noOfWinners > 0
+
+        return JsonResponse({'has_winners': has_winners}, status=status.HTTP_200_OK)
