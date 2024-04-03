72d328acda31a6cc40d52f3e4a7091b8c2fdc2e8
jaalmario <jovanniealmario@gmail.com>
2024-04-03 09:04:48 +0800

    Uncomment lines

--- game/api/base_viewset.py
+++ game/api/base_viewset.py
@@ -13,7 +13,7 @@ class BaseViewSet(viewsets.ViewSet):
     queryset = ()
     serializer_class = ()
     # To implement: JWT auth from the core identity service
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
     @extend_schema(parameters=[OpenApiParameter(name='includeIsDeleted', description='isDeleted filter', type=bool)])
